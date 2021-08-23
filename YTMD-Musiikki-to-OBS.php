b73b07e471e5a50ef2d311c22c1820140b5e4e49
Atte O <mixerboy24@gmail.com>
2021-08-23 05:04:11 +0300

    Create YTMD-Musiikki-to-OBS.php
    Youtube Music Desktop ohjelmasta saadaan API:n kautta "nyt soiva" musiikki esim OBS Studioon. 
    
    Hyödynnän YTMDesktop Remote Controlleria ja Raspberry PI / Linux konetta josta voin OBS:n Selaimella hakea sivun.

---
+++ YTMD-Musiikki-to-OBS.php
@@ -0,0 +1,58 @@
+<?php
+
+$curl = curl_init('http://YTMDESKTOP IP:9863');
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+
+$page = curl_exec($curl);
+if(curl_errno($curl)):
+        echo 'Erro: ' . curl_error($curl);
+        exit;
+endif;
+curl_close($curl);
+
+$DOM = new DOMDocument;
+libxml_use_internal_errors(true);
+
+if(!$DOM->loadHTML($page)):
+        $erros = null;
+        foreach (libxml_get_errors() as $error):
+                $errors.= $error->message."\r\n"; 
+        endforeach;
+
+        libxml_clear_errors();
+        print "LibXML Erros: \r\n$erros";
+        return;
+endif;
+
+$Xpath = new DOMXPath($DOM);
+
+$content = $Xpath->query('//*[@class="truncate"]')->item(0);
+// echo utf8_decode($content->textContent);
+?>
+<html>
+<head>
+<meta http-equiv="refresh" content="5">
+
+<style>
+p {
+  width: 1500px;
+  border: 5px solid white;
+  padding: 50px;
+  margin: 20px;
+  font-size: 50px;
+  color: #ff7200;
+}
+
+
+</style>
+</head>
+
+<body style="background-color: black;">
+<br>
+<br>
+<p>
+Nyt Soi:<br> 
+<?php echo utf8_decode($content->textContent); ?>
+</p>
+</body>
+</html>
